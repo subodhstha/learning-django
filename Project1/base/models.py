c22d704a7efab27c35a02932b4af566f50cfb544
subodhstha <shresthasubodh73@gmail.com>
2023-01-26 09:36:22 +0545

    learing django

--- Project1/base/models.py
+++ Project1/base/models.py
@@ -23,7 +23,7 @@ class Room(models.Model):
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    body = models.TimeField()
+    body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
