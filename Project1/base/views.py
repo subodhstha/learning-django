90085dd182f5f58f7d5763bd440160f3abd48862
subodhstha <shresthasubodh73@gmail.com>
2023-02-27 00:51:16 +0545

    django

--- Project1/base/views.py
+++ Project1/base/views.py
@@ -85,7 +85,10 @@ def room(request, pk):
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
-    context = {'user':user}
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms,'room_messages':room_messages,'topics':topics}
     return render(request, 'base/profile.html',context)
 
 @login_required(login_url='login')
@@ -94,7 +97,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     
     context = {'form': form}
